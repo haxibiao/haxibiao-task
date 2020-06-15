135193e616baa8aa3a54e324fcd2db45b5725624
曾达威 <zengdawei@haxibiao.com>
2020-06-15 22:40:47 +0800

    修复 应用商店好评延迟奖励 job 发放失败

--- src/Jobs/DelayRewaredTask.php
+++ src/Jobs/DelayRewaredTask.php
@@ -7,11 +7,10 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 
 class DelayRewaredTask implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable;
 
     protected $assignment;
 
