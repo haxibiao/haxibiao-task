4d69c8ac91db3cf4cc0ca6c82a39dcb5a88a1448
ivan <ivan@haxibiao.com>
2021-04-29 14:38:18 +0800

    用印象视频的 TaskSeeder做模板，需要覆盖的自己项目seeders下覆盖

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -3,6 +3,7 @@ namespace Database\Seeders;
 
 use Haxibiao\Task\Task;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TaskSeeder extends Seeder
 {
@@ -13,28 +14,28 @@ class TaskSeeder extends Seeder
      */
     public function run()
     {
-        //需要的旧系统手动truncate
-        // DB::table('tasks')->truncate();
+        //正式上线后，不要再清空，要保留运营更新的 图文... (from lara)
+        DB::table('tasks')->truncate();
 
-        //新人任务
         Task::firstOrCreate([
             'name'           => '完善头像',
             'status'         => Task::ENABLE,
             'details'        => '更改账号头像',
+            'group'          => '新人任务',
             'type'           => Task::NEW_USER_TASK,
             'reward'         => array("gold" => "10"),
-            'resolve'        => array('method' => 'checkUserIsUpdateAvatar', 'router' => 'editInformation'),
+            'resolve'        => array("method" => "checkUserIsUpdateAvatar", "router" => "editInformation", "submit_name" => "去修改"),
             'review_flow_id' => 5,
-            'max_count'      => rand(1,10),
         ]);
 
         Task::firstOrCreate([
             'name'           => '视频发布满15个',
+            'group'          => '每日任务',
             'status'         => Task::ENABLE,
             'details'        => '去抖音采集15个视频即可领取奖励',
             'type'           => Task::CUSTOM_TASK,
             'reward'         => array("gold" => "10"),
-            'resolve'        => array('limit' => '15', 'router' => '', 'method' => 'publicArticleTask'),
+            'resolve'        => array('limit' => '15', 'router' => '', 'method' => 'publicArticleTask', 'submit_name' => '去发布'),
             'review_flow_id' => 3,
             'max_count'      => 15,
         ]);
@@ -43,92 +44,79 @@ class TaskSeeder extends Seeder
             'name'           => '绑定手机号',
             'status'         => Task::ENABLE,
             'details'        => '绑定手机号',
+            'group'          => '新人任务',
             'type'           => Task::NEW_USER_TASK,
             'reward'         => array("gold" => "50"),
-            'resolve'        => array('method' => 'checkUserIsUpdatePassAndPhone', 'router' => 'accountBinding'),
+            'resolve'        => array('method' => 'checkUserIsUpdatePassAndPhone', 'router' => 'accountBinding', 'submit_name' => '去绑定'),
             'review_flow_id' => 6,
-            'max_count'      => rand(1,10),
         ]);
 
         Task::firstOrCreate([
             'name'           => '修改性别和生日',
             'status'         => Task::ENABLE,
             'details'        => '修改性别和生日',
+            'group'          => '新人任务',
             'type'           => Task::NEW_USER_TASK,
             'reward'         => array("gold" => "10"),
-            'resolve'        => array('method' => 'checkUserIsUpdateGenderAndBirthday', 'router' => 'editInformation'),
+            'resolve'        => array('method' => 'checkUserIsUpdateGenderAndBirthday', 'router' => 'editInformation', 'submit_name' => '去修改'),
             'review_flow_id' => 7,
-            'max_count'      => rand(1,10),
         ]);
 
         Task::firstOrCreate([
             'name'           => '视频采集悬浮球',
             'details'        => '打开视频采集悬浮球',
+            'group'          => '新人任务',
             'type'           => Task::CUSTOM_TASK,
             'status'         => Task::DISABLE,
-            'resolve'        => array('method' => '', 'router' => 'GoDrinkWater'),
+            'resolve'        => array('submit_name' => '去打开'),
             'review_flow_id' => 0,
-            'max_count'      => rand(1,10),
         ]);
 
-        //自定义任务
         Task::firstOrCreate([
             'name'           => '应用商店好评',
             'details'        => '应用商店好评',
+            'group'          => '新人任务',
             'type'           => Task::CUSTOM_TASK,
             'status'         => Task::DISABLE,
-            'resolve'        => array('method' => '', 'router' => 'ToComment'),
+            'resolve'        => array('method' => '', 'router' => 'ToComment', 'submit_name' => '去好评'),
             'review_flow_id' => 8,
-            'max_count'      => rand(1,10),
         ]);
 
         Task::firstOrCreate([
-            'name'           => '看视频赚钱',
-            'details'        => '看视频有机会获得贡献值哦',
+            'name'           => '有趣小视频',
+            'group'          => '每日任务',
+            'details'        => '看视频有机会获得健康分哦',
             'type'           => Task::DAILY_TASK,
             'status'         => Task::ENABLE,
-            'reward'         => array("gold" => "100", "contribute" => "20"),
-            'resolve'        => array('method' => '', 'router' => 'MotivationalVideo'),
+            'reward'         => array("gold" => "10", "contribute" => "5"),
+            'resolve'        => array('method' => '', 'router' => 'MotivationalVideo', 'submit_name' => '去观看'),
             'review_flow_id' => 4,
             'max_count'      => 10,
         ]);
 
         Task::firstOrCreate([
-            'name'           => '邀请任务',
-            'details'        => '邀请5个新用户，完成注册（需要绑定手机或其他信息）',
-            'type'           => Task::CUSTOM_TASK,
-            'status'         => Task::ENABLE,
-            'reward'         => array("gold" => "600"),
-            'review_flow_id' => 11,
-            'max_count'      => 5,
-        ]);
-        Task::firstOrCreate([
-            'name'           => '直播任务',
-            'details'        => '直播观看人数10+，可领取奖励',
+            'name'           => '回答问题X2',
+            'group'          => '每日任务',
+            'details'        => '回答问题有机会获得奖励哦',
             'type'           => Task::DAILY_TASK,
             'status'         => Task::ENABLE,
-            'reward'         => array("gold" => "200"),
-            'review_flow_id' => 9,
-            'max_count'      => 10,
-        ]);
-        Task::firstOrCreate([
-            'name'           => '作品获赞',
-            'details'        => '作品获赞1000+，可领取奖励',
-            'type'           => Task::DAILY_TASK,
-            'status'         => Task::ENABLE,
-            'reward'         => array("gold" => "600"),
-            'review_flow_id' => 10,
-            'max_count'      => 1000,
+            'reward'         => array("gold" => "30"),
+            'resolve'        => array('submit_name' => '去回答'),
+            'review_flow_id' => 12,
+            'max_count'      => 2,
         ]);
 
         Task::firstOrCreate([
-            'name'           => 'DrinkWaterAll',
-            'details'        => '喝水任务。。。',
+            'name'           => '发布问题X2',
+            'group'          => '每日任务',
+            'details'        => '发布问题有机会获得奖励哦',
             'type'           => Task::DAILY_TASK,
             'status'         => Task::ENABLE,
-            'reward'         => array("gold" => "600"),
-            'review_flow_id' => 10,
-            'max_count'      => 1000,
+            'reward'         => array("gold" => "30"),
+            'resolve'        => array('submit_name' => '去提问'),
+            'review_flow_id' => 13,
+            'max_count'      => 2,
         ]);
+
     }
 }
