af49578ef51014eee1b78763c96a4cef3ffab01c
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-07-15 17:50:43 +0800

    store item方法迁移到task item，慢慢重构item系统

--- src/Traits/ItemRepo.php
+++ src/Traits/ItemRepo.php
@@ -2,7 +2,9 @@
 
 namespace Haxibiao\Task\Traits;
 
+use App\Gold;
 use App\User;
+use Haxibiao\Breeze\Exceptions\GQLException;
 use Haxibiao\Breeze\Exceptions\UserException;
 use Haxibiao\Question\Helpers\Redis\RedisRewardCounter;
 use Haxibiao\Task\Item;
@@ -43,5 +45,44 @@ trait ItemRepo
 
         return $item;
 
+    }
+
+    //领取道具
+    public static function receiveItem($user, $item_id)
+    {
+        if ($user) {
+
+            $item = Item::find($item_id);
+            if ($item) {
+                if ($user->items()->find($item_id)) {
+                    //TODO:已经存在该道具，数量应该叠加
+                    return false;
+                }
+
+                if ($item->price > 0) {
+                    if ($item->price > $user->gold) {
+                        throw new GQLException("金币不足，购买失败！");
+                    }
+                    Gold::makeOutcome($user, $item->value, "购买道具扣除");
+                }
+
+                $user->items()->syncWithoutDetaching([
+                    $item->id => [
+                        "total" => 1,
+                    ],
+                ]);
+                return true;
+            }
+            throw new GQLException("不存在该物品！");
+        }
+        return false;
+    }
+
+    //我的道具
+    public static function myItems()
+    {
+        if ($user = currentUser()) {
+            return $user->items()->get();
+        }
     }
 }
