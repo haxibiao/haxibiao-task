772cf83ad6c67b9ac0531370135c62597777313c
wuxiuzhang <wuxiuzhang@haxibiao.com>
2021-08-03 10:59:19 +0800

    fix:天天答题任务进度检查方法checkDaliyAnswer修改逻辑

--- src/Traits/TaskMethod.php
+++ src/Traits/TaskMethod.php
@@ -370,7 +370,7 @@ trait TaskMethod
         $assignment->resolve      = $resolve;
         return [
             'status'        => $currentCount > 0 && $rewardTicket > $receiveTikect,
-            'current_count' => $todayAnswerCount,
+            'current_count' => $currentCount,
         ];
     }
 
